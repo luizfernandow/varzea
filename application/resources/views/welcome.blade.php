c04d309f6b49918c8276596dd6760e7033b9fe00
Luiz Fernando <luiz0fernando@gmail.com>
2019-06-13 20:04:34 -0300

    Adicionando mapa

--- application/resources/views/welcome.blade.php
+++ application/resources/views/welcome.blade.php
@@ -7,4 +7,8 @@
     @lang('welcome.header')
     <img src="{{ asset('images/logo-150x150.png') }}">
 </div>
+
+<div class="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+    <img class="img-responsive" src="{{ asset('images/mapa.jpeg') }}">
+</div>
 @endsection
