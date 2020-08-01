598a9a9c717e609491ecea3fbd05f7edcb6f4cfd
Luiz Fernando <luiz0fernando@gmail.com>
2020-07-31 21:49:51 -0300

    Adding map to home

--- application/resources/nuxt/pages/index.vue
+++ application/resources/nuxt/pages/index.vue
@@ -1,5 +1,7 @@
 <template>
-    <v-layout column justify-center align-center>
-        <v-flex xs12 sm8 md6> </v-flex>
-    </v-layout>
+    <v-row align="center" justify="center">
+        <v-col cols="12" xs="10">
+            <v-img src="/images/mapa.jpeg" aspect-ratio="2" contain></v-img>
+        </v-col>
+    </v-row>
 </template>
