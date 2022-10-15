65c17b2d850f835c591da334b18efcc9846c5d7d
Luiz Fernando <luiz0fernando@gmail.com>
2022-10-15 19:06:53 -0300

    chore: configs nuxt

--- application/resources/nuxt/nuxt.config.js
+++ application/resources/nuxt/nuxt.config.js
@@ -1,6 +1,5 @@
 import { join } from 'path'
 import fs from 'fs'
-import colors from 'vuetify/es5/util/colors'
 import { copySync, removeSync } from 'fs-extra'
 
 export default {
@@ -128,21 +127,7 @@ export default {
      ** https://github.com/nuxt-community/vuetify-module
      */
     vuetify: {
-        customVariables: ['~/assets/variables.scss'],
-        theme: {
-            dark: true,
-            themes: {
-                dark: {
-                    primary: colors.blue.darken2,
-                    accent: colors.grey.darken3,
-                    secondary: colors.amber.darken3,
-                    info: colors.teal.lighten1,
-                    warning: colors.amber.base,
-                    error: colors.deepOrange.accent4,
-                    success: colors.green.accent3,
-                },
-            },
-        },
+        optionsPath: './vuetify.options.js'
     },
 
     pwa: {
