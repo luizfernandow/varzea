656cac16134cafb94e1a3f2f921c6cf3944ac8a4
Luiz Fernando <luiz0fernando@gmail.com>
2022-10-16 14:41:14 -0300

    fix: style inline

--- application/resources/nuxt/pages/profile.vue
+++ application/resources/nuxt/pages/profile.vue
@@ -68,5 +68,17 @@
 <script>
 export default {
     middleware: 'auth',
+    data: () => ({
+        loading: false,
+        selection: 1,
+    }),
+
+    methods: {
+        reserve() {
+            this.loading = true
+
+            setTimeout(() => (this.loading = false), 2000)
+        },
+    },
 }
 </script>
