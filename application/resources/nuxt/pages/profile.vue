6b0851afaac03a447394a8399882ead586278514
Luiz Fernando <luiz0fernando@gmail.com>
2022-10-16 23:15:49 -0300

    feat: profile test

--- application/resources/nuxt/pages/profile.vue
+++ application/resources/nuxt/pages/profile.vue
@@ -26,12 +26,9 @@
                 <div class="grey--text ms-4">5 (413)</div>
             </v-row>
 
-            <div class="my-4 text-subtitle-1">$ • Italian, Cafe</div>
+            <div class="my-4 text-subtitle-1">Bike</div>
 
-            <div>
-                Small plates, salads & sandwiches - an intimate setting with 12
-                indoor seats plus patio seating.
-            </div>
+            <div>MTB, ROAD</div>
         </v-card-text>
 
         <v-divider class="mx-4"></v-divider>
@@ -44,13 +41,13 @@
                 active-class="deep-purple accent-4 white--text"
                 column
             >
-                <v-chip>5:30PM</v-chip>
+                <v-chip>5:30AM</v-chip>
 
-                <v-chip>7:30PM</v-chip>
+                <v-chip>7:30AM</v-chip>
 
-                <v-chip>8:00PM</v-chip>
+                <v-chip>8:00AM</v-chip>
 
-                <v-chip>9:00PM</v-chip>
+                <v-chip>9:00AM</v-chip>
             </v-chip-group>
         </v-card-text>
 
