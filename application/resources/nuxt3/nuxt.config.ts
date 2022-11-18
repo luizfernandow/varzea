ae7426a4638e46cd909043f97bcf02cc4be07d29
Luiz Fernando <luiz0fernando@gmail.com>
2022-11-18 00:14:44 -0300

    feat: starting migration to nuxt3

--- application/resources/nuxt3/nuxt.config.ts
+++ application/resources/nuxt3/nuxt.config.ts
@@ -1,4 +1,7 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
 
+export default defineNuxtConfig({
+    typescript: {
+        shim: false
+    },
+    ssr: false,
 })
