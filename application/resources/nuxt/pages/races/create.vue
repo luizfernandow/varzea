f16b0a5844a479251d74aa0ac119c2fc95f40fe4
Luiz Fernando <luiz0fernando@gmail.com>
2022-11-19 18:49:42 -0300

    feat: start of create a race

---
+++ application/resources/nuxt/pages/races/create.vue
@@ -0,0 +1,6 @@
+<template><h1>Coming soon</h1></template>
+<script>
+export default {
+    middleware: 'auth',
+}
+</script>
--- application/resources/nuxt/pages/races/index.vue
+++ application/resources/nuxt/pages/races/index.vue
@@ -1,6 +1,20 @@
 <template>
     <v-card class="mx-auto">
-        <v-card-title>{{ $t('races.title') }}</v-card-title>
+        <v-card-title>
+            <p class="ma-0 mr-auto">{{ $t('races.title') }}</p>
+            <v-btn
+                v-if="authenticated"
+                class="mx-2"
+                fab
+                small
+                dark
+                color="indigo"
+                :to="{ name: 'races-create' }"
+            >
+                <v-icon dark> mdi-plus </v-icon>
+            </v-btn>
+        </v-card-title>
+
         <v-list>
             <v-list-item
                 v-for="item in races"
