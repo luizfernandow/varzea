9d3e138a44f69cc4bf56263ed14b7d6ad959ff69
Luiz Fernando <luiz0fernando@gmail.com>
2023-11-15 17:11:44 -0300

    feat: rector laravel

--- application/rector.php
+++ application/rector.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -21,16 +19,9 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/app/Http/Controllers/Controller.php',
     ]);
 
-    $rectorConfig->rules([
-        InlineConstructorDefaultToPropertyRector::class,
-    ]);
+   
 
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
+        LaravelSetList::LARAVEL_100
     ]);
 };
