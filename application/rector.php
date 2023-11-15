9c38bb912bc04bf56a520848a38ddfb5af1b9fa2
Luiz Fernando <luiz0fernando@gmail.com>
2023-11-15 17:17:20 -0300

    feat: enable Opcache JIT (#61)
    * feat: adding JIT opcache
    
    * feat: rector laravel
    
    * feat: rector laravel

--- application/rector.php
+++ application/rector.php
@@ -2,10 +2,8 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\Config\RectorConfig;
-use Rector\Set\ValueObject\LevelSetList;
-use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
@@ -17,20 +15,7 @@ return static function (RectorConfig $rectorConfig): void {
         __DIR__ . '/tests',
     ]);
 
-    $rectorConfig->skip([
-        __DIR__ . '/app/Http/Controllers/Controller.php',
-    ]);
-
-    $rectorConfig->rules([
-        InlineConstructorDefaultToPropertyRector::class,
-    ]);
-
     $rectorConfig->sets([
-        LevelSetList::UP_TO_PHP_82,
-        SetList::CODE_QUALITY,
-        SetList::DEAD_CODE,
-        SetList::EARLY_RETURN,
-        SetList::TYPE_DECLARATION,
-        SetList::PRIVATIZATION,
+        LaravelSetList::LARAVEL_100
     ]);
 };
